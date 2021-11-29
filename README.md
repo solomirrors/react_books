b8c423bba96509ccd7eeec37e984beee3127362e
solomirrors <solomirrors@icloud.com>
2021-11-29 23:37:38 +0100

    Initial react_books

---
+++ README.md
@@ -0,0 +1,1 @@
+React Training Books Application
---
+++ src/components/errors/error-indicator/error-indicator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ErrorIndicator = () => {
+    return(
+        <h1>Error Indicator</h1>
+    )
+}
+
+export default ErrorIndicator;
