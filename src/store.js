13ca37cc35ef0c9fca3f137a2db113666c21dd85
solomirrors <solomirrors@icloud.com>
2021-12-07 20:49:35 +0100

    React Redux Middleware

--- src/store.js
+++ src/store.js
@@ -1,5 +1,21 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const logMiddleware = () => (next) => (action) => {
+    console.log(action.type);
+    return next(action);
+}
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string'){
+        return next({
+            type: action
+        })
+    }
+
+    return next(action)
+}
+
+const store = createStore(reducers, applyMiddleware(stringMiddleware, logMiddleware));
+
 export default store;
